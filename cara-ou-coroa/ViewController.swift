fa6892a0beb9536f4b64d8791ff908c9a1e1188d
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-08-23 16:40:02 -0300

    initial commit

---
+++ cara-ou-coroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cara-ou-coroa
+//
+//  Created by Pedro Léda on 23/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
